2518ec12b8154ab4dacdd4a6566d711d56930e39
root <khubashvili.saba12@gmail.com>
2024-08-08 18:09:08 +0400

    feat

--- src/handlers/websocket/coinflip/coinflip_server.rs
+++ src/handlers/websocket/coinflip/coinflip_server.rs
@@ -4,21 +4,18 @@ use actix::{Actor, Context, Handler, Message, Recipient};
 use rand::Rng;
 use serde::Serialize;
 
-
 //* --- Struct --- */
-
-pub struct CoinflipServer{
-    pub sessions: HashMap<String,CoinflipGame>
+pub struct CoinflipServer {
+    pub sessions: HashMap<String, CoinflipGame>,
 }
 
-
-pub struct CoinflipGame{
-    pub spectators: Vec<Recipient<ClientMessage>>,
+pub struct CoinflipGame {
+    pub spectators: HashMap<i32,Recipient<ClientMessage>>,
     pub players: Vec<Player>,
-    pub amount: f64
+    pub amount: f64,
 }
 #[derive(Clone)]
-pub struct Player{
+pub struct Player {
     pub id: usize,
     pub _name: String,
     pub addr: Recipient<ClientMessage>,
@@ -26,83 +23,115 @@ pub struct Player{
 #[derive(Serialize)]
 pub struct JsonResponse {
     pub message_type: String,
-    pub payload: serde_json::Value
+    pub payload: serde_json::Value,
 }
 //* --- X --- */
-
 //* --- Messages --- */
 #[derive(Message)]
-#[rtype(result="()")]
-pub enum ClientMessage{
+#[rtype(result = "()")]
+pub struct Connect {
+    pub session_id: String,
+    pub user_id: i32,
+    pub addr: Recipient<ClientMessage>,
+}
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Disconnect {
+    pub session_id: String,
+    pub user_id: i32
+}
+#[derive(Message)]
+#[rtype(result = "()")]
+pub enum ClientMessage {
     Text(String),
-    _Json(JsonResponse)
+    _Json(JsonResponse),
 }
 
 #[derive(Message)]
-#[rtype(result="()")]
-pub struct AddGame{
+#[rtype(result = "()")]
+pub struct AddGame {
     pub amount: f64,
-    pub player: Player
+    pub player: Player,
 }
 
 #[derive(Message)]
-#[rtype(result="()")]
-pub struct JoinGame{
+#[rtype(result = "()")]
+pub struct JoinGame {
     pub gameid: String,
     pub player: Player,
 }
 //* X */
 //* --- Actor --- */
-impl Actor for CoinflipServer{
+impl Actor for CoinflipServer {
     type Context = Context<Self>;
 }
-impl Actor for CoinflipGame{
+impl Actor for CoinflipGame {
     type Context = Context<Self>;
 }
 
 //* --- X --- */
-
-
 //* --- Handler --- */
-impl Handler<ClientMessage> for CoinflipGame{
+impl Handler<Connect> for CoinflipServer {
+    type Result = ();
+
+    fn handle(&mut self, msg: Connect, _ctx: &mut Self::Context) -> Self::Result {
+        if let Some(game_session) = self.sessions.get_mut(&msg.session_id) {
+            game_session.spectators.insert(msg.user_id,msg.addr);
+        }
+    }
+}
+impl Handler<Disconnect> for CoinflipServer {
+    type Result = ();
+    fn handle(&mut self, msg: Disconnect, _ctx: &mut Self::Context) -> Self::Result {
+        if let Some(game_session) = self.sessions.get_mut(&msg.session_id) {
+            game_session.spectators.remove(&msg.user_id);
+        }
+    }
+}
+impl Handler<ClientMessage> for CoinflipGame {
     type Result = ();
 
     fn handle(&mut self, msg: ClientMessage, _ctx: &mut Self::Context) -> Self::Result {
-        match msg{
-            ClientMessage::Text(val)=>{
-                for addr in self.sessions.values(){
-                    addr.do_send(ClientMessage::Text(val.clone()))
+        match msg {
+            ClientMessage::Text(val) => {
+                for player in &self.players {
+                    player.addr.do_send(ClientMessage::Text(val.clone()));
                 }
-            },
-            ClientMessage::_Json(val)=>{
-                for addr in self.sessions.values(){
-                    if let Ok(json_val) =  serde_json::to_string(&val){
-                        addr.do_send(ClientMessage::Text(json_val.clone()))
-                    }
+                for spectator in self.spectators.values() {
+                    spectator.do_send(ClientMessage::Text(val.clone()));
+                }
+            }
+            ClientMessage::_Json(val) => {
+                let json_val = serde_json::to_string(&val).unwrap();
+                for player in &self.players {
+                    player.addr.do_send(ClientMessage::Text(json_val.clone()));
+                }
+                for spectator in self.spectators.values() {
+                    spectator.do_send(ClientMessage::Text(json_val.clone()));
                 }
             }
         }
     }
 }
 
-impl Handler<AddGame> for CoinflipServer{
+impl Handler<AddGame> for CoinflipServer {
     type Result = ();
     fn handle(&mut self, msg: AddGame, _ctx: &mut Self::Context) -> Self::Result {
         let id = uuid::Uuid::new_v4().to_string();
-        let new_game = CoinflipGame::new(msg.amount,msg.player);
+        let new_game = CoinflipGame::new(msg.amount, msg.player);
         self.sessions.insert(id, new_game);
     }
 }
 
-impl Handler<JoinGame> for CoinflipServer{
+impl Handler<JoinGame> for CoinflipServer {
     type Result = ();
 
     fn handle(&mut self, msg: JoinGame, _ctx: &mut Self::Context) -> Self::Result {
-        if let Some(game) = self.sessions.get_mut(&msg.gameid){
+        if let Some(game) = self.sessions.get_mut(&msg.gameid) {
             if game.players.iter().all(|p| p.id != msg.player.id) {
-                game.players.insert(msg.player.id,msg.player);
-
-                if game.players.len() == 2{
+                game.players.insert(msg.player.id, msg.player);
+                println!("user joined");
+                if game.players.len() == 2 {
                     game.start_game();
                 }
             }
@@ -111,19 +140,17 @@ impl Handler<JoinGame> for CoinflipServer{
 }
 
 //* --- X --- */
-
 //* Implementations */
-
-impl CoinflipGame{
-    fn new(amount: f64, player: Player) -> Self{
-        let rounded_amount = round_to(amount,2);
-        CoinflipGame{
-            spectators: vec![],
+impl CoinflipGame {
+    fn new(amount: f64, player: Player) -> Self {
+        let rounded_amount = round_to(amount, 2);
+        CoinflipGame {
+            spectators: HashMap::new(),
             players: vec![player],
-            amount: rounded_amount
+            amount: rounded_amount,
         }
     }
-    fn start_game(&mut self){
+    fn start_game(&mut self) {
         let rand_num = rand::thread_rng().gen_range(0..2);
 
         let winner = &self.players[rand_num];
@@ -138,16 +165,13 @@ impl CoinflipGame{
         loser.addr.do_send(ClientMessage::Text(loser_msg.clone()));
 
         // Broadcast the result to all spectators and players
-        for spectator in &self.spectators {
+        for spectator in self.spectators.values() {
             spectator.do_send(ClientMessage::Text(game_result_msg.clone()));
         }
     }
 }
 
-
-
-
-fn round_to(value:f64,decimal_places: u32) -> f64{
+fn round_to(value: f64, decimal_places: u32) -> f64 {
     let factor = 10f64.powi(decimal_places as i32);
     (value * factor).round() / factor
 }
--- src/handlers/websocket/coinflip/coinflip_ws.rs
+++ src/handlers/websocket/coinflip/coinflip_ws.rs
@@ -1,23 +1,108 @@
 use std::time::Duration;
 
 use actix::ActorContext;
+use actix::Addr;
 use actix::AsyncContext;
+use actix::Handler;
+use actix::StreamHandler;
 use actix::{clock::Instant, Actor};
 use actix_web_actors::ws;
+use serde::Deserialize;
+
+
+use super::coinflip_server::JoinGame;
+use super::coinflip_server::Player;
+use super::coinflip_server::{CoinflipServer,Connect,ClientMessage,Disconnect};
 
 pub struct CoinflipWs {
-    pub session_id: i32,
-    // pub addr: Addr<CoinflipServer>,
+    pub session_id: String,
+    pub addr: Addr<CoinflipServer>,
     pub hb: Instant,
     pub user_id: i32,
     pub name: Option<String>,
 }
+
+#[derive(Deserialize)]
+pub struct RequestPayload{
+    msg_type: String,
+    payload: serde_json::Value,
+}
+
+#[derive(Deserialize)]
+pub struct JoinPayload{
+    user_id:usize,
+    game_id:String
+}
+
 
 impl Actor for CoinflipWs {
     type Context = ws::WebsocketContext<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
         self.hb(ctx);
+        self.addr.do_send(Connect{
+            user_id: self.user_id,
+            addr: ctx.address().recipient(),
+            session_id: self.session_id.clone()
+        })
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        self.addr.do_send(Disconnect{
+            user_id:self.user_id,
+            session_id: self.session_id.clone()
+        })
+    }
+}
+
+impl StreamHandler<Result<ws::Message,ws::ProtocolError>> for CoinflipWs{
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg{
+            Ok(ws::Message::Text(txt))=>{
+                if let Ok(msg_type) = serde_json::from_str::<RequestPayload>(&txt){
+                    match msg_type.msg_type.as_str(){
+                        "join"=>{
+                            let payload = serde_json::from_value::<JoinPayload>(msg_type.payload);
+                            match payload{
+                                Ok(user) => {
+                                if let Some(name) = self.name.clone(){
+                                    let new_player = Player{
+                                        id:user.user_id,
+                                        _name: name,
+                                        addr: ctx.address().recipient()
+                                    };
+                                    self.addr.do_send(JoinGame{
+                                        player:new_player,
+                                        gameid:user.game_id.clone()
+                                    })
+                                }
+                                },
+                                Err(_) => {
+                                    println!("Failed to deserialize JoinPayload");
+                                    return;
+                                }
+ 
+                            }
+                        },
+                        _=>()
+                    }
+                }
+            },
+            Ok(ws::Message::Ping(m))=>{
+                self.hb = Instant::now();
+                ctx.pong(&m);
+            },
+            
+            Ok(ws::Message::Pong(_))=>{
+                self.hb = Instant::now();
+            },
+            Err(e)=>{
+                println!("websocket error: {:?}", e);
+                ctx.stop();
+                return;
+            },
+            _=>()
+        }
     }
 }
 
@@ -31,5 +116,22 @@ impl CoinflipWs {
                 ctx.ping(b"");
             }
         });
+    }
+}
+
+
+impl Handler<ClientMessage> for CoinflipWs{
+    type Result = ();
+    fn handle(&mut self, msg: ClientMessage, ctx: &mut Self::Context) -> Self::Result {
+        match msg{
+            ClientMessage::Text(txt)=>{
+                ctx.text(txt);
+            }, 
+            ClientMessage::_Json(jsn)=>{
+                if let Ok(json_msg) = serde_json::to_string(&jsn){
+                    ctx.text(json_msg);
+                }
+            }
+        }
     }
 }
